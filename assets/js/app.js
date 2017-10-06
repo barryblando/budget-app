fccab2bf47593ea7318de9abbf1b1db38098dc1b
heartless007 <barryblando@gmail.com>
2017-10-06 19:11:29 +0800

    Update: Minor Changes 10/06/2017

--- assets/js/app.js
+++ assets/js/app.js
@@ -8,19 +8,19 @@
   }
   * TASK LIST [Planning Step 1]
 
-    * UI MODULE [ VIEW ]: { 
+    * UI MODULE [ VIEW ]: {
       TODO: Get Input Values
       TODO: Add the New Item to the UI
       TODO: Update the UI
     }
-    
-    * DATA MODULE [ MODEL ]: { 
+
+    * DATA MODULE [ MODEL ]: {
       TODO: Add the new Item to Our Data Structure
       TODO: Calculate Budget
     }
 
     //Control the Entire App and acting as a link
-    * CONTROLLER MODULE [ CONTROLLER ]: { 
+    * CONTROLLER MODULE [ CONTROLLER ]: {
       TODO: Add Event Handler
     }
 
@@ -38,13 +38,13 @@
     * TODO: Update the UI
 
     * Event Delegation P.S Important part in JS when it comes manipulating DOM
-      * Event Bubbling - When an event is fired or triggered will also be fired on all the parent elements 
-      * on at a time in a DOM tree until the HTML element which is the root and event that cause to happen is called 
-      * Target Element (e.g Button, etc) - this element is a property in the event object, this means that all the parent elements on which the event will also fire, 
-      * will know the target element of the element, which brings event bubbling to the Event Delegation in DOM tree 
-      * and if know where the event was fired you can simply attach an event handler to a parent element and wait for the event to bubble up, 
+      * Event Bubbling - When an event is fired or triggered will also be fired on all the parent elements
+      * on at a time in a DOM tree until the HTML element which is the root and event that cause to happen is called
+      * Target Element (e.g Button, etc) - this element is a property in the event object, this means that all the parent elements on which the event will also fire,
+      * will know the target element of the element, which brings event bubbling to the Event Delegation in DOM tree
+      * and if know where the event was fired you can simply attach an event handler to a parent element and wait for the event to bubble up,
       * and do whatever you intended to do with the target element.
-      * Event Delegation - is to not set up the event handler on the original element that you're interested in, 
+      * Event Delegation - is to not set up the event handler on the original element that you're interested in,
       * but to attach it to a parent element, basically catch the event there because it bubbles up, and act on the element that you're interested in using the target element property.
 
       * Use Cases for Event Delegation
@@ -52,21 +52,21 @@
         - When you want an event handler attached to an element that is not yet in DOM when our page is loaded.
 
   * TASK LIST [Planning Step 3]
-    * TODO: Calculate Percentages in Expense Column 
-    * TODO: Update Percentages in Expense UI 
+    * TODO: Calculate Percentages in Expense Column
+    * TODO: Update Percentages in Expense UI
     * TODO: Display the Current Month and Year
     * TODO: Number Formatting
     * TODO: Improve input field UX
 
 
 
-  
+
   */
 
 /**
- * 
+ *
   //Using IIFE for data privacy 'cause it creates a new scope that is not visible from the outside scope
-  //and automatically executed when run 
+  //and automatically executed when run
   var budgetController = (function() { //javascript runtime this line here gets executed and this anonymous function is declared and immediately called/invoked 'cause of the operator () in last line of function body
     'use strict';
     //private x and add function, these are in closures even after this IIFE here has returned
@@ -82,17 +82,17 @@
         return add(b);
       }
     };
-  })(); 
+  })();
 
   var UIController = (function() {
     'use strict';
-    
+
   })();
 
   //Controls the budgetController nad UIController
   var appController = (function(budgetCtrl, UICtrl) {
     'use strict';
-    
+
     //store in a variable
     var z = budgetCtrl.publicTest(5); //Good code practice
     // budgetController.publicTes(); <-- Bad code practice
@@ -103,7 +103,7 @@
         console.log(z);
       }
     };
-    
+
   })(budgetController, UIController);
   */
 
@@ -111,7 +111,7 @@
  * BUDGET CONTROLLER
  */
 
-var budgetController = (function() { 
+var budgetController = (function() {
   'use strict';
 
   //<reference https://stackoverflow.com/questions/3350215/what-is-returned-from-a-constructor
@@ -147,8 +147,7 @@ var budgetController = (function() {
   /**
    * store all expenses in array and also for Income
    * we can somehow aggregate a lot of information into one nice data structure
-   * it's always better to have one data structure where all the data goes 
-   * instead of having a lot of random variables flowing around
+   * it's always better to have one data structure where all the data goes 7426462!lastpasvariables flowing around
    */
 
   //private data
@@ -183,7 +182,7 @@ var budgetController = (function() {
     addItem: function(type, des, val) {
       var newItem, ID;
 
-      // ID = last ID + 1 e.g data.allItems['exp'][data.allItems['exp'].length - 1].id + 1 
+      // ID = last ID + 1 e.g data.allItems['exp'][data.allItems['exp'].length - 1].id + 1
       // ID = data.allItems['exp'][data.allItems['exp'].length -1][0] + 1
 
       // Create new ID
@@ -203,7 +202,7 @@ var budgetController = (function() {
       data.allItems[type].push(newItem);
 
       // Return the new element which contains id, description, value
-      return newItem; 
+      return newItem;
     },
     deleteItem: function(type, id) {
       var ids, index;
@@ -232,7 +231,7 @@ var budgetController = (function() {
 
       // TODO : Calculate the Budget: income - expenses
       data.budget = data.totals.inc - data.totals.exp;
-      
+
       // TODO : Calculate the percentage of income that we spent
       /* Expense = 100 and Income 200, spent 50% = 100/200 = 0.5 * 100 */
       /* condition totals should only greater than 0 so it won't return infinity */
@@ -252,7 +251,7 @@ var budgetController = (function() {
       var allPerc = data.allItems.exp.map(function(cur){
         return cur.getPercentage(); //so then this here would get called five times, for each of the five elements, return it and store in allPerc
       });
-      return allPerc; 
+      return allPerc;
     },
     getBudget: function() {
       return {
@@ -266,8 +265,8 @@ var budgetController = (function() {
       console.log(data);
     }
   };
-  
-})(); 
+
+})();
 
 /**
  * UI CONTROLLER
@@ -296,7 +295,7 @@ var UIController = (function() {
   var formatNumber = function(num, type) {
     var numSplit, int, dec, sign;
     /**
-     * before number + or - 
+     * before number + or -
      * exactlty 2 decimal points
      * comma separating the thousands
      * Sample 2310.4567 -> + 2,310.46
@@ -334,7 +333,7 @@ var UIController = (function() {
   //return function so it can be access outside the UIController Scope
   return {
     getInput: function() {
-      return { 
+      return {
         type: document.querySelector(DOMstrings.inputType).value, // will be either income or expense
         description: document.querySelector(DOMstrings.inputDescription).value,
         value: parseFloat(document.querySelector(DOMstrings.inputValue).value) // only integer/float will be accepted in input value
@@ -351,11 +350,11 @@ var UIController = (function() {
         element = DOMstrings.expensesContainer;
         html = '<div class="item clearfix" id="exp-%id%"> <div class="item__description">%description%</div> <div class="right clearfix"> <div class="item__value">%value%</div> <div class="item__percentage">21%</div> <div class="item__delete"> <button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button> </div></div></div>';
       }
-      
+
       //TODO Replace the placeholder text with actual data
       newHtml = html.replace('%id%', obj.id); //search for the id, replace and store new data
       newHtml = newHtml.replace('%description%', obj.description); // search for desc, replace, and store new data
-      newHtml = newHtml.replace('%value%', formatNumber(obj.value)); // search for value, replace, and store new data 
+      newHtml = newHtml.replace('%value%', formatNumber(obj.value)); // search for value, replace, and store new data
 
       //TODO Insert the HTML into the DOM
       //insert the newHtml that holds all the id description value in the element inc or exp
@@ -363,7 +362,7 @@ var UIController = (function() {
       //<reference https://developer.mozilla.org/en-US/docs/Web/API/Element/insertAdjacentHTML
       document.querySelector(element).insertAdjacentHTML('beforeend', newHtml);
     },
-    deleteListItem: function(selectorID) { 
+    deleteListItem: function(selectorID) {
       // delete child element by itself
       var el = document.getElementById(selectorID);
       el.parentNode.removeChild(el);
@@ -381,7 +380,7 @@ var UIController = (function() {
       //we use foreach 'cause it moves over all of the elements of the fields array, and then sets the value of all of them back to empty string
       //<reference https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/forEach
       fieldsArr.forEach(function(current, index, arr) {
-        //set the input.value property (description & value) of these back to empty
+        //set the input.add__value.value property (description too) back to empty
         current.value = '';
       });
 
@@ -399,7 +398,7 @@ var UIController = (function() {
         document.querySelector(DOMstrings.percentageLabel).textContent = obj.percentage + '%';
       } else {
         document.querySelector(DOMstrings.percentageLabel).textContent = '---' ;
-      } 
+      }
     },
     displayPercentages: function(percentages) {
       var fields;
@@ -418,7 +417,7 @@ var UIController = (function() {
       var now, year, month, months;
       now = new Date();
       //christmas = new Date(2017, 11(0 based), 25);
-      months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']; 
+      months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
       month = now.getMonth();
       year =  now.getFullYear();
       document.querySelector(DOMstrings.dateLabel).textContent = months[month] + ' ' + year;
@@ -431,7 +430,7 @@ var UIController = (function() {
         DOMstrings.inputType + ',' +
         DOMstrings.inputDescription + ',' +
         DOMstrings.inputValue
-      ); 
+      );
 
       nodeListForEach(fields, function(cur){
         cur.classList.toggle('red-focus');
@@ -444,7 +443,7 @@ var UIController = (function() {
     }
   };
 })();
- 
+
 /**
  * GLOBAL APP CONTROLLER
  */
@@ -481,18 +480,18 @@ var APPController = (function(budgetCtrl, UICtrl) {
     var input, newItem;
     // TODO : Get the field input data
     input = UIController.getInput();
-    
+
     //isNaN = is not a number is to true, we'll use the not operator ! to make it false so we can get true if it is a number
     if (input.description !== '' && !isNaN(input.value) && input.value > 0) {
       // TODO : Add the item to the budget controller
       newItem = budgetController.addItem(input.type, input.description, input.value);
 
-      // TODO : Add the New item to the UI 
+      // TODO : Add the New item to the UI
       UICtrl.addListItem(newItem, input.type);
-  
+
       // TODO : Clear the fields
       UICtrl.clearFields();
-  
+
       // TODO :  Calculate and Update budget
       updateBudget();
 
@@ -505,7 +504,7 @@ var APPController = (function(budgetCtrl, UICtrl) {
   var ctrlDeleteItem = function(e) {
     var itemID, splitID, type, ID;
     // Event Delegation
-    // Let's test where target element triggered, if you don't use parentNode only the element that is clicked will be returned not the parent element. 
+    // Let's test where target element triggered, if you don't use parentNode only the element that is clicked will be returned not the parent element.
     // Let's do DOM traversing, event bubbling using parentNode 4x to access the id income parent when <i></i> element is triggered
     //console.log(e.target.parentNode.parentNode.parentNode.parentNode.id);
     itemID = e.target.parentNode.parentNode.parentNode.parentNode.id;
@@ -514,12 +513,12 @@ var APPController = (function(budgetCtrl, UICtrl) {
       splitID = itemID.split('-');
       type = splitID[0];
       //convert string to integer
-      ID = parseInt(splitID[1]); 
-      
+      ID = parseInt(splitID[1]);
+
       // TODO : Delete the Item from the data Structure
       budgetCtrl.deleteItem(type, ID);
 
-      // TODO : Delete the Item  from the UI 
+      // TODO : Delete the Item  from the UI
       UICtrl.deleteListItem(itemID);
 
       // TODO : Update and Show the new Budget
@@ -535,12 +534,12 @@ var APPController = (function(budgetCtrl, UICtrl) {
     var DOM = UIController.getDOMstrings();
 
     document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem);
-    
+
     // TODO : Add keypress event (doesn't happen on any specific element, but happens on the global web page)
     document.addEventListener('keypress', function(event) {
       //console.log(event); Get the keyCode
-      if(event.keyCode === 13 || event.which === 13) { 
-        ctrlAddItem(); 
+      if(event.keyCode === 13 || event.which === 13) {
+        ctrlAddItem();
       }
     });
 
@@ -551,7 +550,7 @@ var APPController = (function(budgetCtrl, UICtrl) {
   };
 
   return {
-    //I created it 'cause I want to have a place where I can put all the code that I want to be executed right at the beginning when app starts e.g Eventlisteners 
+    //I created it 'cause I want to have a place where I can put all the code that I want to be executed right at the beginning when app starts e.g Eventlisteners
     init: function() {
       //application has started & reset all values
       UICtrl.displayMonth();
